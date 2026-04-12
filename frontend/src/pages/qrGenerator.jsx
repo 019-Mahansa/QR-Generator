e403358ea00af50b23ac7aa2076ebb4489e37476
019-Mahansa <lightningplay2009@gmail.com>
2026-04-12 14:47:57 +0700

    change for sementara from python lib to js lib

--- frontend/src/pages/qrGenerator.jsx
+++ frontend/src/pages/qrGenerator.jsx
@@ -3,32 +3,43 @@ import './styles/qrGenerator.css'
 import Nav from "../components/nav";
 import { useState } from "react";
 import Footer from "../components/footer";
+import QRCode from "qrcode";
+
 
 function QrGenerator(){
     const [warna,setwarna] = useState("#2A1205")
     const [bgColor,setbgColor] = useState("#FAF7F2")
 
-    const [inputValue, setInputValue] = useState("")
-    const [qrImage, setQrImage] = useState("")
+    const [url,seturl] = useState("")
+    const [qr,setqr] = useState("")
 
-    const handleDownload = async () =>{
-        try {
-        const response = await fetch(qrImage);
-        const blob = await response.blob();
-        const blobUrl = URL.createObjectURL(blob);
+    const generateImage = async () =>{
+        const is = await QRCode.toDataURL(url)
+        setqr(is)
 
-        const link = document.createElement("a");
-        link.href = blobUrl;
-        link.download = "Qr-code Generator.png";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-
-        URL.revokeObjectURL(blobUrl); 
-    } catch (error) {
-        console.error("Gagal download:", error);
     }
-    }
+
+    // const [inputValue, setInputValue] = useState("")
+    // const [qrImage, setQrImage] = useState("")
+
+    // const handleDownload = async () =>{
+    //     try {
+    //     const response = await fetch(qrImage);
+    //     const blob = await response.blob();
+    //     const blobUrl = URL.createObjectURL(blob);
+
+    //     const link = document.createElement("a");
+    //     link.href = blobUrl;
+    //     link.download = "Qr-code Generator.png";
+    //     document.body.appendChild(link);
+    //     link.click();
+    //     document.body.removeChild(link);
+
+    //     URL.revokeObjectURL(blobUrl); 
+    // } catch (error) {
+    //     console.error("Gagal download:", error);
+    // }
+    
 
     return(
         <>
@@ -49,9 +60,8 @@ function QrGenerator(){
                     <span>Enter URL</span>
 
                     <div className="duo">
-                        <input type="text" placeholder="https://example.com" value={inputValue} onChange = {(e) => setInputValue(e.target.value)}></input>
-                        <button className="generate" onClick={() => {    const url = `http://localhost:5000/qr-code?link=${encodeURIComponent(inputValue)}`
-                                                            setQrImage(url)}}> Generate</button>
+                        <input type="text" placeholder="https://example.com" value={url} onChange = {(e) => seturl(e.target.value)}></input>
+                        <button className="generate" onClick={generateImage}> Generate</button>
                     </div>
 
                     <div className="duo">
@@ -66,20 +76,18 @@ function QrGenerator(){
 
                     </div>
                     <div className="place">
-                        {qrImage ? (
-                            <img src={qrImage}  className="w" alt="qr-codes"/>
-                        ): (
+                            { qr ?(
+                            <img src={qr}  className="w" alt="qr-codes"/>
+                            ):(
                             <>
                                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M3 3h7v7H3V3zm2 2v3h3V5H5zm9-2h7v7h-7V3zm2 2v3h3V5h-3zM3 14h7v7H3v-7zm2 2v3h3v-3H5zm13-2h2v2h-2v-2zm-4 0h2v2h-2v-2zm2 2h2v2h-2v-2zm-2 2h2v2h-2v-2zm2 2h2v2h-2v-2zm2-2h2v2h-2v-2z"/>
                                 </svg>
                                 <p>QR code will append in here</p>
-                            </>
-                        )}                 
+                            </>   
+                            )}           
                     </div>
-                    { inputValue.trim() !== "" &&(
-                       <button type="button" className="generate" onClick={handleDownload}>Download</button>
-                    ) }
+                       <button type="button" className="generate" >Download</button>
 
                 </div>
 
